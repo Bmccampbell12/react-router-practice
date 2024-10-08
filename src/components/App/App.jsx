f30f73c52fe9c894a14ebbe422e7bc6d4ee88801
Bruce McCampbell <mccampbellbruce1@gmail.com>
2024-10-07 20:34:46 -0500

    - imported HashRouter as Router statement into App.jsx.
    - Added <nav> element for links to home, about, and students in App.jsx file.
    
    - Moved “App” div to below function.
    
    - Created routes for home, about, and students files.
    
    - Created components for Home, About, and Students.

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,18 +2,51 @@ import './App.css';
 import StudentForm from '../StudentForm/StudentForm.jsx';
 import StudentList from '../StudentList/StudentList.jsx';
 import { HashRouter as Router, Route, Link } from 'react-router-dom';
+import About from './About';
 
 function App() {
     return (
         <div className="App">
             <header>
                 <h1>GitHub Student List</h1>
-                {/* Nice spot for a nav, eh? */}
-            </header>
-            <StudentForm />
-            <StudentList />
+                </header>
+        <Router>
+            
+                <ul>
+                    <li>
+                        <link to="home">Home</link>
+                    </li>
+                     <li>
+                       <link to="/about">About</link>
+                     </li>
+                    <li>
+                        <link to="/students">Students</link>
+                    </li>
+                </ul>
+            
+
+      
+        
+               <Route path="/" exact>
+                    <Home />
+                 </Route>
+               <Route path="/about" exact>
+                    <About />
+                 </Route>
+               <Route path="/students" exact>
+                     <Students />
+                 </Route>
+                 <Route path="/StudentForm" exact>
+                    <StudentForm />
+                 </Route>
+                 <Route path="/StudentForm" exact>
+                     <StudentList />
+                 </Route>
+               </Router>
         </div>
+    
     );
+    
 }
 
 
